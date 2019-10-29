a6e579203657b51e309408ad90de32a4ec871eb3
bringforthjoy <adelugba.emma@gmail.com>
2019-10-29 19:55:10 +0100

    Default User Auth

--- app/User.php
+++ app/User.php
@@ -1,24 +1,19 @@
 <?php
-
 namespace App;
-
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
 class User extends Authenticatable
 {
     use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'full_name', 'username', 'email', 'password',
     ];
-
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -27,7 +22,6 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
     /**
      * The attributes that should be cast to native types.
      *
--- app/posts.php
+++ app/posts.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class posts extends Model
+class Posts extends Model
 {
     //
 }
