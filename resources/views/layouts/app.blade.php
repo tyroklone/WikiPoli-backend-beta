f12e6e20d21f5845ef3fae8f1ff2e502093b45e2
HP ENVY 17 <HP ENVY 17@KIZINHO>
2019-10-31 16:34:15 +0100

    user dashboard done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,117 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        @yield('title')
+        <link rel="stylesheet" href="styles/main.css" />
+        <!-- bootstrap -->
+        <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            />
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+            />
+        <link rel="icon" type="img/png" href="{{ asset('images/favicon.png') }}">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <span id="logo"
+                          ><a href=""
+                        ><img
+                                src="https://res.cloudinary.com/fabianuzukwu/image/upload/v1571749198/c09e9odiqy2cvkosfubl.png"
+                                alt=""
+                                width="100"/></a
+                        ></span>
+                </div>
+                <div class="col-md-6">
+                    <ul class="firstnav">
+                        <li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: inline-block;">
+                                @csrf
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+                                <button type="submit" id="btn" class="btn btn-sm btn-primary">LogOut</button>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> 
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
+                            </form>
+                        </li>
+                        <li>
+                            <a href=""><i class="fa fa-user"></i>&nbsp;{{Auth::user()->full_name}}</a>
+                        </li>
+                        <li><a href="">Settings</a></li>
+                        <li>
+                            <a href="" style="color: red;"
+                               ><span>3</span><i class="fa fa-bell-o"></i
+                                ></a>
+                        </li>
                     </ul>
                 </div>
             </div>
-        </nav>
 
-        <main class="py-4">
+            <div class="row">
+                <div class="col-md-7">
+                    <a href="" id="create"><i class="fa fa-user"></i>&nbsp;{{Auth::user()->full_name}}</a>
+                </div>
+                <div class="col-md-5">
+                    <div class="input-group mb-3" style="text-align-last: inherit;">
+                        <a href="{{url('create-post')}}" id="create">Create a post <i class="fa fa-plus"></i></a
+                        >&nbsp;&nbsp;&nbsp;&nbsp;
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="input"
+                            placeholder="Enter Politicial Name"
+                            />
+                        <div class="input-group-append">
+                            <span class="input-group-text">Search</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
             @yield('content')
-        </main>
-    </div>
-</body>
+
+
+
+            <div id="footer">
+                <div class="container" >
+                    <div class="row links">
+                        <div class="col-md-4 col-sm-12  text-center">
+                            <ul class="quick-links">
+                                <li>Legal</li>
+                                <li><a href="Terms-of-use.html">Terms of Use</a></li>
+                                <li><a href="privacypolicy.html">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4 col-sm-12  text-center">
+                            <ul class="quick-links">
+                                <li>Company</li>
+                                <li><a href="">About</a></li>
+                                <li><a href="">Careers</a></li>
+                                <li><a href="">FAQs</a></li>
+                                <li><a href="">Donations</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-md-4 col-sm-12 text-center">
+                            <h3 style="font-size:17px; padding-top: 14px;">Connect With Us</h3>
+                            <div class="social-link">
+                                <ul>
+                                    <li><a href="facebook.com"><span class="fa fa-facebook"></span></a> </li>
+                                    <li><a href="twitter.com"><span class="fa fa-twitter"></span></a></li>
+                                    <li><a href="instagram.com"><span class="fa fa-instagram"></span></a> </li>
+                                </ul>                                   
+                                <p>contact@wikipoli.gq</p>
+                            </div> 
+                        </div>
+                        <div class="col-lg-12 copyright text-center">
+                            <p>&copy; 2019 Wikipoli - All Rights Reserved</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </body>
 </html>
