d7adf516e3f6d5abdc8d6ef3a81270d6221f6dfb
TheresaNde <31648818+TheresaNde@users.noreply.github.com>
2019-10-31 10:03:06 +0100

    Add files via upload

---
+++ resources/views/tour.blade.php
@@ -0,0 +1,171 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/styles/tour_style.css') }}"/>
+    <link rel="icon" type="img/png" href="images/favicon.png">
+    <title>Tour</title>
+  </head>
+  <body>
+
+        <nav class="navbar navbar-expand-lg navbar-custom bg-custom">
+                <div class="container">
+                    <a href="index.html" class="logo"><img src="https://res.cloudinary.com/siyfa/image/upload/v1571745363/u0zhq4fwslfr4u5lx5qt.png" alt="logo" class="img-fluid" width="150px;"></a>
+                    <button class="navbar-toggler float-right custom-toggler" type="button" data-toggle="collapse" data-target="#navbar9" style="color: #000000;">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="navbar-collapse collapse" id="navbar9">
+                        <ul class="navbar-nav ml-auto"> 
+                            <li class="navbar-item mr-5">
+                                <a href="signin.html" class="navbar-link" style="color: black;" id="logIn">Log In</a>
+                            </li>
+                            <li class="navbar-item mr-5">
+                                <a href="signup.html" class="btn navbar-link" id="signUp" style="background-color: #6d9bf1;">Sign Up</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            
+            <div class="search">
+                <form action="action_page.php">
+                    <input type="text" name="search" placeholder="Enter Politician's name" class="justify-content-right">
+                    <button type="submit" class="button" name="Search">Search</button>
+                </form>
+            </div>
+        
+    <div class= "wrapper">
+
+      <header>
+          <h4><b>HOW IT WORKS</b></h4>
+      </header>
+      <div class="container">
+        
+        <div class="row">
+            <div class="col-lg-5">
+                <article class="first-article">
+                    <section class="article-header">WikiPoli</section>
+                    <section class="article-body">
+                        <p>Do you have information about any political candidate in your area,
+                           say Senetor Jagaga is starting his journey in politics and you'd like
+                           people to know about him? WikiPoli is your safe choice.
+                        <br>
+                        Do you have concrete information about your opposition and you'd want the world to know
+                            Honorable Koinkoin deep secret?
+                        <br>
+                        Follow this steps and in no time you'd be able to add politicians
+                           in your area and even edit information about them.</p>
+                    </section>
+                </article>
+            </div>
+
+            <div class="col-lg-2"></div>
+
+            <div class="col-lg-5">
+              <img src="./image/undraw_newspaper_k72w.svg" alt="newspaper" width="400px" height="400px" >
+            </div>
+        </div>
+    
+
+        <div class="row">
+
+            <div class="col-lg-5">
+                <img src="./image/undraw_mobile_login_ikmv.svg" alt="sign-up" width="400px" height="400px">
+            </div>
+
+            <div class="col-lg-2 eclipse-row">
+                <p class="step">STEP <br>
+                <span class="number">01</span></p>
+                <img src="./image/Ellipse 31.svg" alt="eclipse" class="eclipse">
+                <img src="./image/Ellipse 31.svg" alt="eclipse" class="eclipse">
+                <img src="./image/Ellipse 31.svg" alt="eclipse" class="eclipse">
+                <img src="./image/Ellipse 31.svg" alt="eclipse" class="eclipse">
+                <img src="./image/Ellipse 31.svg" alt="eclipse" class="eclipse">
+
+            </div>
+            <div class="col-lg-5 sign">
+                 <section class="article-header"><a href="#">  Sign up NOW! </a></section>
+                 <section class="article-body">
+                <p>All you have to do is create an account for yourselve, there are more interesting features when you do,
+                    you can comment on a post about a politician, edit and even use your favourite emoticon and much more...
+                <br>
+                Don't limit yourself to just reading, do more!</p>
+                 </section>
+            </div>
+
+        </div>
+
+        <div class="row">
+
+                <div class="col-lg-5">
+                    <article>
+                      <section class="article-header">  Almost there! </section>
+                      <section class="article-body">
+                        <p>Once your account has been created, you now have access to create posts about your 
+                            favourite politician. You can edit posts made by others and even comments while using emoticons.
+                        </p>
+                      </section>
+                    </article>    
+                </div>
+                <div class="col-lg-2 eclipse-row">
+                        <p class="step">STEP <br>
+                        <span class="number">02</span></p>
+                </div>
+
+                <div class="col-lg-5">
+                        <img src="./image/undraw_post_online_dkuk.svg" alt="sign-up" class="sign-up" width="400px" height="400px">
+                    </div>
+
+
+
+        </div>
+        </div>
+        <div style="clear: both"></div>
+    <div class="login-footer">
+
+        <div class="footer-links">
+            <div class="left">
+                <ul>
+                    <li><a href="#"><strong>Support</strong></a></li>
+                    <li><a href="#">Contact us</a></li>
+                    <li><a href="./Terms-of-use.html"">Terms of use</a></li>
+                    <li><a href="./privacypolicy.html">Privacy Policy</a></li>
+                    <li><a href="donation_page.html">Donations</a></li>
+                </ul>
+            </div>
+           <div class="right">
+                <ul>
+                    <li><a href="#"><strong>Company</strong></a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="careers.html">Careers</a></li>
+                    <li><a href="tour.html">tour</a></li>
+                </ul> 
+           </div>
+           <div class="clear" style="clear: both"></div>
+            <ul class="icon-area">  
+                <li class="icon-area"><a href=""><i class="fab fa-facebook-f"></i></a></li>
+                <li class="icon-area"><a href=""><i class="fab fa-twitter"></i></a></li>
+                <li class="icon-area"><a href=""><i class="fab fa-instagram"></i></a></li>
+                <li class="icon-area"><a href=""><i class="fab fa-linkedin-in"></i></a></li>
+                <li class="icon-area"><a href=""><i class="fab fa-pinterest-p"></i></a></li>
+            </ul>
+            <p>2019 WikiPoli. All Rights Reserved</p>
+        </div>
+       
+    
+    </div>
+            
+        
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
