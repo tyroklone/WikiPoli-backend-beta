99d2a35e4f47012a3c5b228aff5d15d062e25300
bringforthjoy <adelugba.emma@gmail.com>
2019-10-31 09:14:24 +0100

    fixed 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -14,10 +14,94 @@ or https://adenikesalau.github.io/404-error-page/#  click on any of the links to
 
         <link rel="stylesheet" type="text/css" href="{{ asset('css/styles/404.css') }}"/>
 
+        
+
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato">
         <link rel="icon" type="img/png" href="images/favicon.png">
 
         <title> ERROR 404 </title>
+
+        <style>
+        body{
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: normal;
+}
+
+.container{
+    height: 100%;
+    width: 100%;
+}
+
+.error-template{
+    text-align: center;
+    padding: 2%;
+}
+
+.error-action{
+    margin: 10px;
+}
+
+.error-action .btn{
+    margin-right: 15px;
+}
+
+.icon{
+    width: 150px;
+    height: 150px;
+    margin-left: 5%;
+}
+
+.error-img{
+    width: 700px;
+    height: 300px;
+}
+
+.error-details{
+    margin-bottom: 3%;
+}
+
+.button{
+    background-color: #6D9BF1;
+    border: 1px  #6D9BF1;
+    padding: 1% 7%;
+    cursor: pointer;
+}
+
+a {
+    text-decoration: none;
+    color: #ffebef;
+    font-weight: normal;
+}
+
+@media screen and (max-width: 800px){
+    .error-template{
+        padding-top: 10%;
+    }
+    
+    .icon{
+        width: 25%;
+        height: 25%;
+        margin-left: 0px;
+    }
+
+    .error-img{
+        width: 70%;
+        height: 20%;
+    }
+}
+
+@media screen and (max-width: 480px){
+    .container{
+        padding-top: 3%;
+    }
+
+    .error-img{
+        padding-top: 20%;
+    }
+}
+
+        </style>
     </head>
     
     <body>
@@ -41,7 +125,7 @@ or https://adenikesalau.github.io/404-error-page/#  click on any of the links to
 
                     <!--link to take user to home page-->
                     
-                    <a href="index.html" class="button">Back To Home</a>
+                    <a href="{{ url('/') }}" class="button">Back To Home</a>
                     
                 </div>
 
